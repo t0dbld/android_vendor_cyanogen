b53b68ad9cb3272a440bbff3dbd6cd92d933896a
Emilio López <turl@tuxfamily.org>
2011-08-08 18:51:20 -0300

    Correct model naming

--- products/cyanogen_olympus.mk
+++ products/cyanogen_olympus.mk
@@ -13,7 +13,7 @@ $(call inherit-product, vendor/cyanogen/products/gsm.mk)
 PRODUCT_NAME := cyanogen_olympus
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := olympus
-PRODUCT_MODEL := Motorola Atrix 4G MB860
+PRODUCT_MODEL := MB860
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=olyatt BUILD_ID=4.5.91 BUILD_DISPLAY_ID=4.5.91 BUILD_FINGERPRINT=MOTO/olyatt/olympus:2.3.4/4.5.91/110625:user/release-keys PRIVATE_BUILD_DESC="olympus-user 2.3.4 4.5.91 110625 release-keys"
 
